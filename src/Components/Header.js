714fe6f5de3744e90e38a6fddba9de6db9fbe532
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-11-01 23:46:50 -0600

    final commit before midnight 11/1/2020

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 export default function Header() {
     return (
         <Jumbotron style={{backgroundColor: "#cc4400"}}>
-            <h1 style={{backgroundColor: "#cc4400"}} className="jumbotron" >Pumpkin Spice<a style={{color: "#1f6135"}} href="https://www.linkedin.com/in/jess-white-chicago/">*</a> Quiz App</h1>
+            <h1 style={{backgroundColor: "#cc4400"}} className="jumbotron" >Pumpkin Spice<a style={{color: "#52c779"}} href="https://www.linkedin.com/in/jess-white-chicago/">*</a> Quiz App</h1>
         </Jumbotron>
     )
 }
