4d608d6ddffefe1ac79d0ceb8f37d2c6ba27985e
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-12-05 16:52:21 -0600

    changed testing file

--- src/App.test.js
+++ src/App.test.js
@@ -14,104 +14,104 @@ Enzyme.configure({ adapter: new Adapter() })
 
 //component tests:
 
-// describe('TriviaRoundComponent', () => {
-//   it('should contain a Card tag', () => {
-//     const wrapper = shallow(<TriviaRound/>)
-//     expect(wrapper.containsAnyMatchingElements([
-//   <progress></progress>
-// ])).toEqual(true);
-//   })
-// })
+describe('TriviaRoundComponent', () => {
+  it('should contain a Card tag', () => {
+    const wrapper = shallow(<TriviaRound/>)
+    expect(wrapper.containsAnyMatchingElements([
+  <progress></progress>
+])).toEqual(true);
+  })
+})
 
 // //function tests:
 
-// describe('createQuizArrayEqual', () => {
-//   it('should return an array that is equal to QUESTIONDATA', () => {
-//     const quizArray = createQuizArray()
-//     expect(isEqual(quizArray, QUESTIONDATA)).toEqual(true)
-//   })
-// })
+describe('createQuizArrayEqual', () => {
+  it('should return an array that is equal to QUESTIONDATA', () => {
+    const quizArray = createQuizArray()
+    expect(isEqual(quizArray, QUESTIONDATA)).toEqual(true)
+  })
+})
 
-// describe('createQuizArrayItemEqual', () => {
-//   it('should return an array whose first value is equal to the first value of QUESTIONDATA', () => {
-//     const quizArray = createQuizArray() 
-//     expect(isEqual(quizArray[0], QUESTIONDATA[0])).toEqual(true)
-//   })
-// })
+describe('createQuizArrayItemEqual', () => {
+  it('should return an array whose first value is equal to the first value of QUESTIONDATA', () => {
+    const quizArray = createQuizArray() 
+    expect(isEqual(quizArray[0], QUESTIONDATA[0])).toEqual(true)
+  })
+})
 
-// describe('createQuizArrayItemValueEqual', () => {
-//   it('should return an array whose first value has an "incorrect" array that is equal to the same array in QUESTIONDATA', () => {
-//     const quizArray = createQuizArray() 
-//     expect(quizArray[0].incorrect === QUESTIONDATA[0].incorrect).toEqual(true)
-//   })
-// })
+describe('createQuizArrayItemValueEqual', () => {
+  it('should return an array whose first value has an "incorrect" array that is equal to the same array in QUESTIONDATA', () => {
+    const quizArray = createQuizArray() 
+    expect(quizArray[0].incorrect === QUESTIONDATA[0].incorrect).toEqual(true)
+  })
+})
 
-// describe('createQuizArrayLengthEqual', () => {
-//   it('should return an array that is the same length as QUESTIONDATA', () => {
-//     const quizArray = createQuizArray() 
-//     expect(quizArray.length === QUESTIONDATA.length).toEqual(true)
-//   }) 
-// })
+describe('createQuizArrayLengthEqual', () => {
+  it('should return an array that is the same length as QUESTIONDATA', () => {
+    const quizArray = createQuizArray() 
+    expect(quizArray.length === QUESTIONDATA.length).toEqual(true)
+  }) 
+})
 
-// describe('createRandomizedArray', () => {
-//   it('should return an array that is NOT equal to the original QUESTIONDATA', () => {
-//     const randomArray = randomizeQuestions()
-//     expect(isEqual(randomArray, QUESTIONDATA)).toEqual(false)
-//   })
-// })
+describe('createRandomizedArray', () => {
+  it('should return an array that is NOT equal to the original QUESTIONDATA', () => {
+    const randomArray = randomizeQuestions()
+    expect(isEqual(randomArray, QUESTIONDATA)).toEqual(false)
+  })
+})
 
-// describe('createRandomizedArrayLength', () => {
-//   it('should return an array that is the same length of the original QUESTIONDATA', () => {
-//     const randomArray = randomizeQuestions() 
-//     expect(randomArray.length === QUESTIONDATA.length).toEqual(true)
-//   })
-// })
+describe('createRandomizedArrayLength', () => {
+  it('should return an array that is the same length of the original QUESTIONDATA', () => {
+    const randomArray = randomizeQuestions() 
+    expect(randomArray.length === QUESTIONDATA.length).toEqual(true)
+  })
+})
 
-// describe('createRandomizedArrayUnequal', () => {
-//   it('should return an array that is not in the same order as the original QUESTIONDATA', () => {
-//     const randomArray = randomizeQuestions()
-//     let counter = 0 
-//     let randomTester = 0
-//     while (counter < randomArray.length) {
-//       if ((isEqual(randomArray[counter], QUESTIONDATA[counter])) === true) {
-//         randomTester += 1
-//       }
-//       counter += 1
-//     }
-//     expect(randomTester < (QUESTIONDATA.length)/2).toEqual(true)
-//   })
-// })
+describe('createRandomizedArrayUnequal', () => {
+  it('should return an array that is not in the same order as the original QUESTIONDATA', () => {
+    const randomArray = randomizeQuestions()
+    let counter = 0 
+    let randomTester = 0
+    while (counter < randomArray.length) {
+      if ((isEqual(randomArray[counter], QUESTIONDATA[counter])) === true) {
+        randomTester += 1
+      }
+      counter += 1
+    }
+    expect(randomTester < (QUESTIONDATA.length)/2).toEqual(true)
+  })
+})
 
-// describe('triviaRoundLength', () => {
-//   it('should return an array that contains 10 questions', () => {
-//     const triviaRound = getTriviaRound() 
-//     expect(triviaRound.length === 10).toEqual(true)
-//   });
-// })
+describe('triviaRoundLength', () => {
+  it('should return an array that contains 10 questions', () => {
+    const triviaRound = getTriviaRound() 
+    expect(triviaRound.length === 10).toEqual(true)
+  });
+})
 
-// describe('addAnswersArray', () => {
-//   it('should add an array called answers to each question', () => {
-//     const withAnswerArray = reformatQuestions()
-//     const testQuestion = withAnswerArray[0] 
-//     expect(testQuestion.answers !== false).toEqual(true)
-//   })
-// })
+describe('addAnswersArray', () => {
+  it('should add an array called answers to each question', () => {
+    const withAnswerArray = reformatQuestions()
+    const testQuestion = withAnswerArray[0] 
+    expect(testQuestion.answers !== false).toEqual(true)
+  })
+})
 
-// describe('reformatAnswers', () => {
-//   it('should add an array called answers that contains all four possible answers', () => {
-//     const withAnswerArray = reformatQuestions()
-//     const testQuestion = withAnswerArray[0] 
-//     expect(testQuestion.answers.length === 4).toEqual(true)
-//   })
-// })
+describe('reformatAnswers', () => {
+  it('should add an array called answers that contains all four possible answers', () => {
+    const withAnswerArray = reformatQuestions()
+    const testQuestion = withAnswerArray[0] 
+    expect(testQuestion.answers.length === 4).toEqual(true)
+  })
+})
 
-// describe('pushCorrectAnswerIntoAnswerArray', () => {
-//   it('should push the correct answer into the answers array', () => {
-//     const withAnswerArray = reformatQuestions()
-//     const testQuestion = withAnswerArray[0] 
-//     expect(testQuestion.answers.includes(testQuestion.correct)).toEqual(true)
-//   })
-// })
+describe('pushCorrectAnswerIntoAnswerArray', () => {
+  it('should push the correct answer into the answers array', () => {
+    const withAnswerArray = reformatQuestions()
+    const testQuestion = withAnswerArray[0] 
+    expect(testQuestion.answers.includes(testQuestion.correct)).toEqual(true)
+  })
+})
 
 // //component tests:
 
@@ -175,52 +175,52 @@ Enzyme.configure({ adapter: new Adapter() })
 //   }) 
 // })
 
-//Win test: Take in a string input "2 3 11 + 5 - *" and 
-//return a single integer that is the postfix sum of all numbers and operands in the input, 18
+Win test: Take in a string input "2 3 11 + 5 - *" and 
+return a single integer that is the postfix sum of all numbers and operands in the input, 18
 
-// describe('finalTest', () => {
-//   const calculate = (expression) => {
+describe('finalTest', () => {
+  const calculate = (expression) => {
     
-//     const ourArray = [];
-//     const operands = ["+", "-", "*"]
-//     expression.split(" ").forEach(element => {
-//       // if element is +, -, *, then do whatever
-//       if (operands.includes(element)) {
-//         const right = ourArray.pop();
-//         const left = ourArray.pop();
-//         // do addition if the operand is "+"
-//         if (element === "+") {
-//           ourArray.push(right + left)
-//         } else if (element === "-") {
-//           ourArray.push(left - right)
-//         } else if (element === "*") {
-//           ourArray.push(left * right)
-//         }
-//       } else {
-//         ourArray.push(parseInt(element))
-//       }
-//       // else we know it's a number so we can convert to an integer and put it in the array
-//       // parseInt 
-//     });
+    const ourArray = [];
+    const operands = ["+", "-", "*"]
+    expression.split(" ").forEach(element => {
+      // if element is +, -, *, then do whatever
+      if (operands.includes(element)) {
+        const right = ourArray.pop();
+        const left = ourArray.pop();
+        // do addition if the operand is "+"
+        if (element === "+") {
+          ourArray.push(right + left)
+        } else if (element === "-") {
+          ourArray.push(left - right)
+        } else if (element === "*") {
+          ourArray.push(left * right)
+        }
+      } else {
+        ourArray.push(parseInt(element))
+      }
+      // else we know it's a number so we can convert to an integer and put it in the array
+      // parseInt 
+    });
     
-//     return ourArray[0];
-//   };
+    return ourArray[0];
+  };
 
-//   it('should take in a string input "1 2 +" and return a single integer, 3', () => {
-//     const finalTestSum = calculate("1 2 +");
-//     expect(finalTestSum).toEqual(3);
-//   });
-//   it('should take in a string input "1 2 -" and return a single integer, -1', () => {
-//     const finalTestSum = calculate("1 2 -");
-//     expect(finalTestSum).toEqual(-1);
-//   });
-//   it('should take in a string input "1 2 *" and return a single integer, 2', () => {
-//     const finalTestSum = calculate("1 2 *");
-//     expect(finalTestSum).toEqual(2);
-//   });
-//   it('should take in a string input "2 3 11 + 5 - *" and return a single integer, 18', () => {
-//     const finalTestSum = calculate("2 3 11 + 5 - *");
-//     expect(finalTestSum).toEqual(18);
-//   });
-// });
+  it('should take in a string input "1 2 +" and return a single integer, 3', () => {
+    const finalTestSum = calculate("1 2 +");
+    expect(finalTestSum).toEqual(3);
+  });
+  it('should take in a string input "1 2 -" and return a single integer, -1', () => {
+    const finalTestSum = calculate("1 2 -");
+    expect(finalTestSum).toEqual(-1);
+  });
+  it('should take in a string input "1 2 *" and return a single integer, 2', () => {
+    const finalTestSum = calculate("1 2 *");
+    expect(finalTestSum).toEqual(2);
+  });
+  it('should take in a string input "2 3 11 + 5 - *" and return a single integer, 18', () => {
+    const finalTestSum = calculate("2 3 11 + 5 - *");
+    expect(finalTestSum).toEqual(18);
+  });
+});
 
